df7ac79de659c5e0625ab004eb36623f40ca6e59
Jarigyani <jarigyani.b@gmail.com>
2024-04-22 17:01:33 +0900

    :sparkles: 背景を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { SeparatedBG } from "./components/SeparatedBG";
+
 function App() {
 	return (
-		<>
+		<div className="h-[200vh]">
 			<button type="button" className="btn">
 				click me
 			</button>
-		</>
+			<SeparatedBG />
+		</div>
 	);
 }
 
---
+++ src/components/SeparatedBG.tsx
@@ -0,0 +1,7 @@
+export const SeparatedBG = () => {
+	return (
+		<div className="w-screen h-screen fixed top-0 left-0 -z-10 flex flex-col justify-end">
+			<div className="h-[50%] bg-green-200" />
+		</div>
+	);
+};

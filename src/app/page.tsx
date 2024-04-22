c65f5b3b5d24024b0d36377a93014747047eb7c5
Jarigyani <jarigyani.b@gmail.com>
2024-04-22 16:25:03 +0900

    :fire: Nextごみ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div>
+		<div className="">
 			<p>root page</p>
 			<div>
 				<p>Links</p>
@@ -10,6 +10,7 @@ export default function Home() {
 					<p>about</p>
 				</Link>
 			</div>
+			<div className="w-8 h-8 bg-red-500 fixed top-0" />
 		</div>
 	);
 }
